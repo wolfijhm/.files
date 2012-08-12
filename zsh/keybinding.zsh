5e7fb6d16f2df7c140a3f28ae08342c71644f765
Yannik Stein <yannikstein@gmail.com>
2012-08-12 22:12:39 +0200

    enhance zsh
    * vi-like keybindings in menu completion
    * theme: show hostname truncated to the first period
    * introduce EDITOR variable
    * more keybindings to kill text

--- zsh/keybinding.zsh
+++ zsh/keybinding.zsh
@@ -1,25 +1,38 @@
+# emacs like keybindings
 bindkey -e
 
-# del
+# del -> delete character
 bindkey "[3~" delete-char
-# CTRL-a
+# CTRL-a -> jump to the beginning of the line
 bindkey "OH" beginning-of-line
-# CTRL-e
+# CTRL-e -> jump to the end of line
 bindkey "OF" end-of-line
-# ALT-<Right>
+# ALT-<Right> -> forward word
 bindkey ";5C" emacs-forward-word
 bindkey "OC" emacs-forward-word
-# ALT-<Left>
+# ALT-<Left> -> backward word
 bindkey ";5D" emacs-backward-word
 bindkey "OD" emacs-backward-word
-# ALT-m
+# CTRL-B -> kill previous word
+bindkey "" backward-kill-word
+# ALT-m -> duplicate previous word
 bindkey "m" copy-prev-shell-word
+# CTRL-^ -> kill to the beginning of the line
+bindkey "" backward-kill-line
 
-# CTRL-X_e
+# CTRL-X_e -> edit command line
 autoload edit-command-line && zle -N edit-command-line
 bindkey "" edit-command-line
 
-# from grml-zsh: CTRL-z
+# grml-zsh {{{
+# CTRL-z -> reexecute background job
 bindkey -s "" "fg\n"
+# use the vi navigation keys (hjkl) besides cursor keys in menu completion
+zmodload zsh/complist
+bindkey -M menuselect 'h' vi-backward-char        # left
+bindkey -M menuselect 'k' vi-up-line-or-history   # up
+bindkey -M menuselect 'l' vi-forward-char         # right
+bindkey -M menuselect 'j' vi-down-line-or-history # bottom
+# }}}
 
-# vim: ft=zsh
+# vim: ft=zsh foldmethod=marker

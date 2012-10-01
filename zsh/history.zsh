1cb2a5485b7d0c1899218c14806b29e2f0a49808
Yannik Stein <yannik.stein@gmail.com>
2012-10-01 16:01:37 +0200

    fix aliases for 1-9(in zsh:go dir back in history)

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -6,7 +6,13 @@ setopt appendhistory
 setopt hist_ignore_all_dups
 
 DIRSTACKSIZE=8
-setopt autopushd # use pushd instead of cd
-setopt pushdignoredups # ignore duplicates in the directory history
-setopt pushdsilent # print no message
+# use pushd instead of cd
+setopt autopushd
+# ignore duplicates in the directory history
+setopt pushdignoredups
+# print no message
+setopt pushdsilent
+# invert meaning of + and - (highest element in the stack hast highest order)
+setopt pushdminus
+# push directories relative to home dir
 setopt pushdtohome

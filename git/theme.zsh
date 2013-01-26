0d3e09c97597b7bb8a3e65ea8be9aa1f025850b4
Yannik Stein <yannik.stein@gmail.com>
2013-01-26 14:00:05 +0100

    fix bug in zsh-git-theme
    The colors reflected only the state of the repository for the current
    subdirectory.

--- git/theme.zsh
+++ git/theme.zsh
@@ -1,20 +1,28 @@
 # based on
 # https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
 
+goto_gitroot() {
+  [[ -d '.git' || $(pwd) == '/' ]] && return
+  cd ..
+  goto_gitroot
+}
+
 git_updateprompt() {
   local ref prefix suffix color clean_color dirty_color
-  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  ref=$(git symbolic-ref HEAD 2> /dev/null)
   ref=${ref#refs/heads/}
+
+  [[ -z $ref ]] && return
 
   prefix="<git:"
   suffix=">"
-  clean_color="cyan"
-  dirty_color="yellow"
   untracked_color="red"
+  dirty_color="yellow"
+  clean_color="cyan"
 
-  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+  if [[ -n $(goto_gitroot; git ls-files --other --exclude-standard 2> /dev/null) ]]; then
     color="$untracked_color"
-  elif [[ -n $(git status -s 2> /dev/null) ]]; then
+  elif [[ -n $(goto_gitroot; git status -s 2> /dev/null) ]]; then
     color="$dirty_color"
   else
     color="$clean_color"

6e62fadf5bb9a2b6d604b45cce3118938cd221f3
Yannik Stein <yannik.stein@gmail.com>
2013-04-23 16:47:38 +0200

    color man pages

---
+++ zsh/man.zsh
@@ -0,0 +1,10 @@
+# colors for man pages
+# source: http://nion.modprobe.de/blog/archives/572-less-colors-for-man-pages.html
+
+export LESS_TERMCAP_mb=$'\E[01;31m'
+export LESS_TERMCAP_md=$'\E[01;36m'
+export LESS_TERMCAP_me=$'\E[0m'
+export LESS_TERMCAP_se=$'\E[0m'
+export LESS_TERMCAP_so=$'\E[01;44;33m'
+export LESS_TERMCAP_ue=$'\E[0m'
+export LESS_TERMCAP_us=$'\E[01;37m'
